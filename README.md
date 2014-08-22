6302efbe47d4b2d883ed974d8a0a81b7fc369cad
cnbeining <cnbeining@gmail.com>
2014-08-22 05:54:22 -0400

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,68 @@ audioblacker
 ============
 
 Yet another video patch tool to bypass Letvcloud's encode.
+
+This cheat would not add video time, which is better than VFR(https://github.com/cnbeining/FlvPatcher  ). Also quicker and use less space.
+
+Requirement
+-------
+
+- Python 2.7
+- ffmpeg
+- ffprobe
+- Enough spare space: the size of the original video plus 2 (3 if use safe mode with auto-cleaning failed) times of the audio stream.
+
+Usage
+------
+
+    python audioblacker.py (-h) (-i input.mp4) (-o output.mp4) (-b 1990000) (-a 120000) (-s 1)
+    
+    -h: Default: None
+        Help.
+    
+    -i: Default: Blank
+        Input file.
+        If the file and audioblacker are not under the same path,
+        it is suggested to use absolute path to avoid possible failure.
+    
+    -o Default: input-filename.black.mp4
+       Output file.
+       Would be in the same folder with the original file if not specified.
+       
+    -b: Default: 1990000
+        Target bitrate.
+    
+    -a: Default: 110000
+        Target audio bitrate.
+        
+    audioblacker would calculate both of the required black time,
+    and choose the larger one to make sure your convert is successful.
+    
+    -s: Default: 1
+        Use safe mode.
+        audioblacker would check whether the file's audio is AAC.
+        Disabling would save you some space and time,
+        if you know what you are doing.
+
+
+Author
+-----
+
+Beining, http://www.cnbeining.com/
+
+License
+-----
+
+GNUv2 license.
+
+Misc
+-----
+
+- You are not supposed to refer/mention/promote this software at any service within Chinese Mainland.
+
+- This method is enlightened by @七音弦樱 and @LYF.
+
+History
+----
+
+0.1: The very beginning
---
+++ audioblacker.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+#coding:utf-8
+# Author:  Beining --<ACICFG>
+# Contact: http://www.cnbeining.com/   |https://github.com/cnbeining/audioblacker
+# Purpose: Patch video's audio part to bypass Letvcloud's transcode.
+# Created: 08/21/2014
+# LICENSE: GNU v2
+
+import sys
+import os
+import os, sys, subprocess, shlex, re
+from subprocess import call
+import uuid
+import math
+import shutil
+import getopt
+
+
+
+#----------------------------------------------------------------------
+def probe_file(filename):
+    cmnd = ['ffprobe', '-show_format', '-pretty', '-loglevel', 'quiet', filename]
+    p = subprocess.Popen(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #print filename
+    out, err =  p.communicate()
+    #print out
+    if err:
+        print err
+        return None
+    return out
+
+
+#----------------------------------------------------------------------
+def time_to_sec(time_raw):
+    """
+    str->int
+    
+    ignore .*."""
+    hr = int(time_raw.split(':')[0]) * 3600
+    minute = int(time_raw.split(':')[1]) * 60
+    sec = int(float(time_raw.split(':')[2]))
+    return int(hr + minute + sec)
+
+#----------------------------------------------------------------------
+def get_abspath(filename):
+    """"""
+    return str(os.path.abspath(filename))
+
+#----------------------------------------------------------------------
+def process(filename, target_bitrate, audiobitrate, safe, outputfile):
+    """str,int,int,str->?
+    filename,outputfile comes with the path.
+    safe=0: directly convert the file to m4a"""
+    tmpdir = '/tmp/Audioblacker-' + str(uuid.uuid4())
+    if not os.path.exists(tmpdir):
+        os.makedirs(tmpdir)
+    audio_format = ''
+    audio_duration = ''
+    audio_bitrate = ''
+    video_format = ''
+    video_duration = ''
+    video_bitrate = ''
+    #demux audio file
+    # Add this to save time, for experienced users
+    print('INFO: Checking audio and remux...')
+    if safe == 0:
+        #directly to m4a to save space
+        os.system('ffmpeg -i \'' + filename + '\' -vn -c:a copy ' + tmpdir+'/audio.m4a' +'> /dev/null 2>&1')
+        try:
+            for line in probe_file(tmpdir+'/audio.m4a').split('\n'):
+                #print(line)
+                if 'duration' in line:
+                    audio_duration = str(line.split('=')[1])
+        except:
+            print('ERROR: Cannot read audio file!')
+            exit()
+    else:
+        os.system('ffmpeg -i \'' + filename + '\' -vn -c:a copy ' + tmpdir+'/audio.aac' +'> /dev/null 2>&1')
+        try:
+            for line in probe_file(tmpdir+'/audio.aac').split('\n'):
+                    if 'format_name' in line:
+                        audio_format = str(line.split('=')[1])
+                    if 'duration' in line:
+                        audio_duration = str(line.split('=')[1])
+            print('INFO: Audio safe, converting to m4a and clean cache...')
+            os.system('ffmpeg -i '+ tmpdir+'/audio.aac -vn -c:a copy ' + tmpdir+'/audio.m4a' +'> /dev/null 2>&1')
+            try:
+                os.remove(tmpdir+'/audio.aac')
+            except:
+                print('WARNING: Cannot remove the aac file now...')
+        except:
+            print('ERROR: Cannot read audio file!')
+            exit()
+        #In case someone screw the audio up
+        if not 'aac' in audio_format:
+            print('ERROR: You have to use AAC as audio format!')
+            exit()
+    #Check original file
+    try:
+        for line in probe_file(filename).split('\n'):
+                if 'duration' in line:
+                    video_duration = str(line.split('=')[1])
+    except:
+        print('ERROR: Cannot read video file!')
+        exit()
+    #Calc...
+    #time_by_video
+    print('INFO: Doing calculation...')
+    video_duration_sec = time_to_sec(video_duration)
+    video_size_byte = int(os.path.getsize(filename))
+    audio_duration_sec = time_to_sec(audio_duration)
+    audio_size_byte = int(os.path.getsize(tmpdir+'/audio.m4a'))
+    #!!!!!FLOAT DUE TO ACCURACY!!!!!
+    target_byterate = target_bitrate / 8
+    target_audiobyterate = audiobitrate / 8 
+    time_video = int(math.ceil((video_size_byte - target_byterate * video_duration_sec) / (target_byterate - 300)))
+    time_audio = int(math.ceil((audio_size_byte - target_audiobyterate * audio_duration_sec) / (target_audiobyterate - 300)))
+    if time_audio < 0 and time_video < 0:
+        print('ERROR: Cannot calculate target, your target bitrate is higher than the original file!')
+        shutil.rmtree(tmpdir)
+        exit()
+    if time_audio == 0 and time_video == 0:
+        time_patch = 60
+    elif time_audio > time_video:
+        time_patch = time_audio + 10
+    else:
+        time_patch = time_video + 10
+    #Make audio patch
+    f = open(tmpdir + '/audiocode.txt', 'w')
+    ff = 'file \'' + tmpdir + '/audio.m4a\'' + '\n'
+    os.getcwd()
+    py_path = sys.path[0]
+    os.chdir(py_path)
+    for i in range(time_patch):
+        ff = ff + 'file \'' + str(os.getcwd()) + '/'+ '1sblack.m4a\'' + '\n'
+    ff = ff.encode("utf8")
+    f.write(ff)
+    f.close()
+    print('INFO: Concating audios...')
+    os.system('ffmpeg -f concat -i '+ tmpdir + '/audiocode.txt -c copy '+ tmpdir +'/audiopatched.m4a'+'> /dev/null 2>&1')
+    print('INFO: Making final output file...')
+    os.system('ffmpeg -i ' + filename + ' -i ' + tmpdir +'/audiopatched.m4a -c copy  -map 0:0 -map 1:0 '+outputfile +'> /dev/null 2>&1')
+    print('Done!')
+    #clean up
+    try:
+        shutil.rmtree(tmpdir)
+    except:
+        print('ERROR: Cannot remove temp dir, do it by yourself!')
+
+#----------------------------------------------------------------------
+def usage():
+    """"""
+    print('''Usage:
+    
+    python audioblacker.py (-h) (-i input.mp4) (-o output.mp4) (-b 1990000) (-a 120000) (-s 1)
+    
+    -h: Default: None
+        Help.
+    
+    -i: Default: Blank
+        Input file.
+        If the file and audioblacker are not under the same path,
+        it is suggested to use absolute path to avoid possible failure.
+    
+    -o Default: input-filename.black.mp4
+       Output file.
+       Would be in the same folder with the original file if not specified.
+       
+    -b: Default: 1990000
+        Target bitrate.
+    
+    -a: Default: 110000
+        Target audio bitrate.
+        
+    audioblacker would calculate both of the required black time,
+    and choose the larger one to make sure your convert is successful.
+    
+    -s: Default: 1
+        Use safe mode.
+        audioblacker would check whether the file's audio is AAC.
+        Disabling would save you some space and time,
+        if you know what you are doing.
+        ''')
+
+
+#----------------------------------------------------------------------
+if __name__=='__main__':
+    argv_list = []
+    argv_list = sys.argv[1:]
+    filename = ''
+    target_bitrate = 1990000
+    outputfile = ''
+    safe = 1
+    audiobitrate = 110000
+    try:
+        opts, args = getopt.getopt(argv_list, "hi:b:a:o:s:", ['help', 'input','bitrate', 'audiobitrate'
+                                                           'outputfile', 'safe'])
+    except getopt.GetoptError:
+        usage()
+        exit()
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage()
+            exit()
+        elif o in ('-i', '--input'):
+            filename = a
+            try:
+                argv_list.remove('-i')
+            except:
+                break
+        elif o in ('-b', '--bitrate'):
+            target_bitrate = int(a)
+            try:
+                argv_list.remove('-b')
+            except:
+                break
+        elif o in ('-a', '--audiobitrate'):
+            audiobitrate = int(a)
+            try:
+                argv_list.remove('-a')
+            except:
+                break        
+        elif o in ('-o', '--outputfile'):
+            outputfile = a
+            try:
+                argv_list.remove('-o')
+            except:
+                break
+        elif o in ('-s', '--safe'):
+            safe = int(a)
+            try:
+                argv_list.remove('-s')
+            except:
+                break
+    if filename == '':
+        print('ERROR: No input file!')
+        exit()
+    if outputfile == '':
+        outputfile = filename.split('.')[0] + '.black.mp4'
+    process(filename, target_bitrate, audiobitrate, safe, outputfile)
